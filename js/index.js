784f97430243bb792d155b0b769401c9d8a5de37
Rémy DAVID <remy.david@etu.univ-lyon1.fr>
2021-11-25 15:34:06 +0100

    Adding js on nav

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("myNav").style.display = "block";
+    document.getElementById("croix").style.display = "flex";
+  }
+
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myNav").style.display = "none";
+    document.getElementById("croix").style.display = "none";
+  }
